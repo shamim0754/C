372cf1d6dc38c7b58383335bfc00d6cbd6cd6e21
Md.Shamim Miah <shamim.ict0754@gmail.com>
2016-12-24 23:19:59 +0600

    ddd

--- hello.c
+++ hello.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <conio.h>
+
+struct Date  
+{  
+   int dd;  
+   int mm;  
+   int yyyy;   
+};
 struct employee  {   
     int id;  
     char name[50];  
-    float salary;  
+    float salary; 
+    struct Date joindate; 
+    /*
+    or
+    struct joinDate  
+    {  
+      int dd;  
+      int mm;  
+      int yyyy;   
+    }joindate;
+    */   
 };
 void main(){
-    struct employee emp[2]; 
-    int i;
-    for(i = 0;i < 2;i++){
-        scanf("%d",&emp[i].id);
-        scanf("%s",&emp[i].name);
-        scanf("%f",&emp[i].salary);
-    }
-
-    //print
-    for(i = 0;i < 2;i++){
-        printf("%d",emp[i].id);
-        printf("%s",emp[i].name);
-        printf("%f\ln",emp[i].salary);
-    }
+    struct employee e1; 
+    e1.id=101; // or e1->id=101
+    strcpy(e1.name, "Sonoo Jaiswal");
+    e1.salary=25000;
+    e1.joindate.dd=10;  
+    e1.joindate.mm=11;  
+    e1.joindate.yyyy=2014;
+    printf( "employee id : %d\n", e1.id);  
+    printf( "employee id : %d\n", e1.joindate.dd);  
+    printf( "employee name : %s\n", e1.name);        
+    printf( "employee salary : %f\n", e1.salary);  
 
 }
--- readme.md
+++ readme.md
@@ -259,6 +259,49 @@ void main(){
 
 ```
 
+### Nested Structure ###
+
+```C
+#include <stdio.h>
+#include <conio.h>
+
+struct Date  
+{  
+   int dd;  
+   int mm;  
+   int yyyy;   
+};
+struct employee  {   
+    int id;  
+    char name[50];  
+    float salary; 
+    struct Date joindate; 
+    /*
+    or
+    struct joinDate  
+    {  
+      int dd;  
+      int mm;  
+      int yyyy;   
+    }joindate;
+    */   
+};
+void main(){
+    struct employee e1; 
+    e1.id=101; // or e1->id=101
+    strcpy(e1.name, "Sonoo Jaiswal");
+    e1.salary=25000;
+    e1.joindate.dd=10;  
+    e1.joindate.mm=11;  
+    e1.joindate.yyyy=2014;
+    printf( "employee id : %d\n", e1.id);  
+    printf( "employee id : %d\n", e1.joindate.dd);  
+    printf( "employee name : %s\n", e1.name);        
+    printf( "employee salary : %f\n", e1.salary);  
+
+}
+``` 
+
 ### Structures Array ###
 
 ```C
